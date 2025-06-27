9617dadbd07ac73f40aedd315ea987212424df25
pranavpenugonda <92200984+pranavpenugonda@users.noreply.github.com>
2025-06-27 13:04:21 +0530

    Delete CartService directory

--- CartService/src/main/java/com/CartService/exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.CartService.exception;
-
-public class ProductNotFoundException extends Exception {
-
-	public ProductNotFoundException(String msg) {
-		super(msg);
-	}
-	
-}
--- CartService/src/main/java/com/CartService/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.CartService.exception;
-
-public class UserNotFoundException extends Exception {
-
-	public UserNotFoundException(String msg) {
-		super(msg);
-	}
-	
-}
